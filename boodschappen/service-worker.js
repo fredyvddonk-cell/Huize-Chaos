6f3b879cf3b89afcc747c529a02676a6655c0833
fredyvddonk-cell <fredy.vd.donk@gmail.com>
2026-08-26 08:58:41 +0200

    V1.3.147 – boncontrole vernieuwd
    Totaalbedrag bon blijft gelijk aan kassabontotaal.
    Boncontrole uitgesplitst naar YNAB-categorieën.
    Koopzegels gekocht en betaald met koopzegels apart meegerekend.
    Berekend en ingevoerd totaal direct vergeleken.
    Duidelijke melding Bon klopt / Verschil.
    Koopzegelvelden op mobiel netjes uitgelijnd.

--- boodschappen/service-worker.js
+++ boodschappen/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='boodschappen-v1-3-144-pwa-1';
-const APP_ASSETS=['./','./index.html','./style.css?v=1.3.144','./js/app.js?v=1.3.144','./js/shopping.js?v=1.3.144','./js/stock.js?v=1.3.144','./js/hutsel.js?v=1.3.144','./js/insight.js?v=1.3.144','./js/firebase.js?v=1.3.144','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/icon-maskable-512.png'];
+const CACHE_NAME='boodschappen-v1-3-147-pwa-1';
+const APP_ASSETS=['./','./index.html','./style.css?v=1.3.147','./js/app.js?v=1.3.147','./js/shopping.js?v=1.3.147','./js/stock.js?v=1.3.147','./js/hutsel.js?v=1.3.147','./js/insight.js?v=1.3.147','./js/firebase.js?v=1.3.147','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/icon-maskable-512.png'];
 const SHARED_RECEIPT_CACHE='huize-chaos-shared-receipts-v1';
 async function handleSharedReceipt(request){
   const formData=await request.formData();
